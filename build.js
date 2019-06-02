2e128b759d1f5323aacd666457ff0b29710672fc
b1f6c1c4 <b1f6c1c4@gmail.com>
2019-06-02 12:38:07 -0400

    favicon

--- build.js
+++ build.js
@@ -11,10 +11,10 @@ async function makeBundle() {
   ]);
 
   await bundler.bundle();
-  // const html = shell.cat('art/html_code.html').replace(/\n/g, '');
-  // shell.sed('-i', /<title>/, `${html}<title>`, 'dist/*.html');
+  const html = shell.cat('art/html_code.html').replace(/\n/g, '');
+  shell.sed('-i', /<title>/, `${html}<title>`, 'dist/*.html');
   shell.sed('-i', /Workaround/, '', 'dist/*.html');
-  // shell.cp('art/*', 'dist/');
+  shell.cp('art/*', 'dist/');
   shell.cp('-r', 'bin/', 'dist/');
   shell
     .echo('https://crypto-sm.netlify.com/* https://sm.b1f6c1c4.info/:splat 301!')
